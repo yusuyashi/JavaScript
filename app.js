1a0a391788d4b98a7af67a1c4caacd1c55046361
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2024-04-14 06:34:09 +0000

    JavaScript関数じゃんけん

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-//メイン部分
-let alertString;
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-//作成した関数を呼び出し、変数へ格納
-alertString = addStiring("WebCamp");
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
 
-//変数の中身をアラートで表示する
-alert(alertString);
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
 
-//作成した関数
-function addStiring(strA){
-    let addStr = "Hello" + strA;
-    return addStr;
+// 結果を表示する
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
 }
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
+  
